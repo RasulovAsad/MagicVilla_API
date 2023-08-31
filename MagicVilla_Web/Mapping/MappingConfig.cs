4d4612605b4d977484473183586fc97d113a1e08
RasulovAsad <tu1spghy7@code.edu.az>
2023-08-31 15:47:33 +0400

    Added Web project

---
+++ MagicVilla_Web/Mapping/MappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MagicVilla_Web.Models.DTO;
+
+namespace MagicVilla_Web.Mapping
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<VillaDTO, VillaCreateDTO>().ReverseMap();
+            CreateMap<VillaDTO, VillaUpdateDTO>().ReverseMap();
+
+            CreateMap<VillaNumberDTO, VillaNumberCreateDTO>().ReverseMap();
+            CreateMap<VillaNumberDTO, VillaNumberUpdateDTO>().ReverseMap();
+        }
+    }
+}
